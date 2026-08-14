e5205af581b5d3ede54244fb80e50d7e2653fabf
hydai <z54981220@gmail.com>
2026-08-15 01:30:38 +0800

    refactor(app): remove unused streamer href hook

--- app/hooks/useStreamerHref.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { useStreamer } from '../contexts/StreamerContext';
-
-export function useStreamerHref(path: string): string {
-  const { slug } = useStreamer();
-  return `/${slug}${path}`;
-}
